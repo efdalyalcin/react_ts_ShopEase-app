5f82f43ebcea4e122ab43a34964a4195d879045e
efdalyalcin <efdalyalcin@hotmail.com>
2023-06-08 16:57:36 +0300

    readme, axios, getproducts, type products created

---
+++ src/services/getProducts.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import { BASE_URL } from './urls';
+import { IProduct } from 'src/types/product.type';
+
+export const getProducts = () => {
+  return new Promise<IProduct>((resolve, reject) => {
+    axios
+      .get(`${BASE_URL}/products`)
+      .then((res) => resolve(res.data))
+      .catch((error) => reject(error));
+  });
+};
