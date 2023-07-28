67f95b0a0f89fe147df128f9b5f532f10cb81a7d
efdalyalcin <efdalyalcin@hotmail.com>
2023-07-28 15:35:11 +0300

    getCategories created and implemented on search bar

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -2,13 +2,19 @@ import { useCallback, useEffect, useState } from 'react';
 import './SearchBar.scss';
 import { Link } from 'react-router-dom';
 import cn from 'classnames';
+import { useQuery } from 'react-query';
+import { getCategories } from 'src/services/getCategories';
+import { nanoid } from 'nanoid';
 
 export default function SearchBar() {
   const [isSearchable, setIsSearchable] = useState(false);
   const [searchInput, setSearchInput] = useState('');
 
-  // this is a custom hook from the context
-  // const { searchInput, setSearchInput } = useSearchHandler();
+  // put all categories in the begining of data, find a way
+  const { isLoading, isError, data, error } = useQuery({
+    queryKey: ['categories'],
+    queryFn: getCategories,
+  });
 
   useEffect(() => {
     if (searchInput.length > 2) {
@@ -33,6 +39,14 @@ export default function SearchBar() {
     [isSearchable]
   );
 
+  if (isError) return <div>{`Error on the server: ${error}`}</div>;
+  if (isLoading)
+    return (
+      <div className="loading">
+        <h1 className="loading__text">Loading...</h1>
+      </div>
+    );
+
   return (
     <div className="SearchBar">
       <div className="SearchBar__bar">
@@ -46,8 +60,9 @@ export default function SearchBar() {
         />
       </div>
       <select>
-        <option>sdja</option>
-        <option>sdjasdds</option>
+        {data?.map((category) => {
+          return <option key={nanoid()}>{category}</option>;
+        })}
       </select>
       <Link
         to="/react_ts-search-user-app/search"
