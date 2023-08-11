7775addab0101661c30d8f140448e92703f7e6d6
efdalyalcin <efdalyalcin@hotmail.com>
2023-08-11 13:37:50 +0300

    page size max min center fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Route, Router, Routes } from 'react-router-dom';
-import Home from './pages/Home';
+import Home from './pages/Home/Home';
 import SearchPage from './pages/SearchPage';
 
 function App() {
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,10 @@
 import Header from 'src/components/Header/Header';
 import Recommended from 'src/components/Recommended/Recommended';
+import './Home.scss';
 
 export default function Home() {
   return (
-    <section>
+    <section className="home-page">
       <Header />
       <Recommended />
     </section>
