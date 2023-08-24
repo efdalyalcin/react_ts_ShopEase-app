2918622c3f1f3e0cf6d1e574c255119232e16b4a
efdalyalcin <efdalyalcin@hotmail.com>
2023-08-24 14:59:38 +0200

    catalog folders created

---
+++ src/components/Catalog/Catalog.tsx
@@ -0,0 +1,9 @@
+import CatalogCard from '../CatalogCard/CatalogCard';
+
+export default function Catalog() {
+  return (
+    <section>
+      <CatalogCard />
+    </section>
+  );
+}
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 import Header from 'src/components/Header/Header';
 import Recommended from 'src/components/Recommended/Recommended';
 import './Home.scss';
+import Catalog from 'src/components/Catalog/Catalog';
 
 export default function Home() {
   return (
     <section className="home-page">
       <Header />
+      <Catalog />
       <Recommended />
     </section>
   );
