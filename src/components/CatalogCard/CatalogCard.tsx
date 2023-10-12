a521de50e4a8c56a85780afdb3859d0e975c6988
efdalyalcin <efdalyalcin@hotmail.com>
2023-10-12 19:24:16 +0200

    card title shortener func

--- src/components/CatalogCard/CatalogCard.tsx
+++ src/components/CatalogCard/CatalogCard.tsx
@@ -4,10 +4,15 @@ type Props = {
   title: string;
 };
 
+const makeTwoWordsTitle = (title: string): string => {
+  const wordsArr = title.split(' ');
+  return `${wordsArr[0]} ${wordsArr[1]}`;
+};
+
 export default function CatalogCard({ title }: Props) {
   return (
     <div className="catalog-card">
-      <p className="catalog-card__title">{title}</p>
+      <p className="catalog-card__title">{makeTwoWordsTitle(title)}</p>
       <div className="catalog-card__image"></div>
     </div>
   );
