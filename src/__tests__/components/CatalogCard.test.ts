702751928da995e3d087b88f47f46c209196569a
efdalyalcin <efdalyalcin@hotmail.com>
2023-10-28 14:29:09 +0200

    new catalog card tests

--- src/__tests__/components/CatalogCard.test.ts
+++ src/__tests__/components/CatalogCard.test.ts
@@ -16,4 +16,18 @@ describe('<Catalog Card />', () => {
     );
     expect(getByText('This is')).toBeTruthy();
   });
+
+  it('should display the price like "From ... EURO"', () => {
+    const { getByText } = render(
+      CatalogCard({ title: '', img: '', price: '10' })
+    );
+    expect(getByText('From 10 EURO')).toBeTruthy();
+  });
+
+  it('should render with invalid props', () => {
+    const { container } = render(
+      CatalogCard({ title: 'Title', img: 'invalid', price: '10' })
+    );
+    expect(container.getElementsByClassName('catalog-card')).toHaveLength(1);
+  });
 });
