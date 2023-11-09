fc4b3238f4cab2dfef9b9b019b63854c918655a8
efdalyalcin <efdalyalcin@hotmail.com>
2023-11-09 17:59:30 +0100

    getProducts test start

---
+++ src/__tests__/functions/getProducts.test.ts
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import { getProducts } from 'src/services/getProducts';
+import { describe, expect, it, vi } from 'vitest';
+
+describe('getProducts', () => {
+  it('should return a promise that resolves to an array of products when successful', () => {
+    // Mock axios.get to return a resolved promise with mock data
+    vi.spyOn(axios, 'get').mockResolvedValue({
+      data: [
+        {
+          id: 1,
+          title: 'Product 1',
+          price: '10',
+          category: 'Category 1',
+          description: 'Description 1',
+          image: 'image1.jpg',
+        },
+      ],
+    });
+
+    // Call the getProducts function
+    const result = getProducts();
+
+    // Assert that the result is a promise
+    expect(result).toBeInstanceOf(Promise);
+
+    // Assert that the promise resolves to an array of products
+    return expect(result).resolves.toEqual([
+      {
+        id: 1,
+        title: 'Product 1',
+        price: '10',
+        category: 'Category 1',
+        description: 'Description 1',
+        image: 'image1.jpg',
+      },
+    ]);
+  });
+});
