3f4d53d1dbdac27602a722f062fc79432e78a88e
efdalyalcin <efdalyalcin@hotmail.com>
2024-02-24 16:20:43 +0100

    test: fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from 'src/App.tsx';
-import './reset.scss';
-import './global.scss';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from 'src/App.tsx'
+import './reset.scss'
+import './global.scss'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
         <App />
-      </BrowserRouter>
-    </QueryClientProvider>
-  </React.StrictMode>
-);
+      </QueryClientProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+)
