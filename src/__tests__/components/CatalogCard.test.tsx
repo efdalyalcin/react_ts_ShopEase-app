a2c9ab0d4b524c0a38b094658eeaac1651a1d462
efdalyalcin <efdalyalcin@hotmail.com>
2024-02-24 16:14:12 +0100

    test: test fix

---
+++ src/__tests__/components/CatalogCard.test.tsx
@@ -0,0 +1,42 @@
+import { describe, it, expect } from 'vitest'
+import { render } from '@testing-library/react'
+import CatalogCard from 'src/components/CatalogCard/CatalogCard'
+
+describe('<Catalog Card />', () => {
+  it('should render a div with class "catalog-card"', () => {
+    const { container } = render(
+      <CatalogCard title="" img="" price="" category="" />,
+    )
+    expect(container.getElementsByClassName('catalog-card')).toHaveLength(1)
+  })
+
+  it('should display the formatted title when the title has more than two words', () => {
+    const { getByText } = render(
+      <CatalogCard title="This is a long title" img="" price="" category="" />,
+    )
+    expect(getByText('This is')).toBeTruthy()
+  })
+
+  it('should display the price like "From ... EURO"', () => {
+    const { getByText } = render(
+      <CatalogCard title="" img="" price="10" category="" />,
+    )
+    expect(getByText('From 10 EURO')).toBeTruthy()
+  })
+
+  it('should render with invalid props', () => {
+    const { container } = render(
+      <CatalogCard title="title" img="invalid" price="10" category="" />,
+    )
+    expect(container.getElementsByClassName('catalog-card')).toHaveLength(1)
+  })
+
+  it('should render the price element when price is missing', () => {
+    const { container } = render(
+      <CatalogCard title="title" img="image.jpg" price="" category="" />,
+    )
+    expect(
+      container.getElementsByClassName('catalog-card__price'),
+    ).toHaveLength(1)
+  })
+})
